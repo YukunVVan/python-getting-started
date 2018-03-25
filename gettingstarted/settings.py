087f5eac067741df4019deb0d6826116ae9fdaaa
YukunVVan <31962749+YukunVVan@users.noreply.github.com>
2018-03-25 03:08:45 -0400

    allowedhosts

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -29,19 +29,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','cryptic-stream-41447.herokuapp.com']
 
-# STATICFILES_DIRS = [
-#     "./hello/static"
-# ]
+STATICFILES_DIRS = [
+    "./hello/static"
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Application definition
 
